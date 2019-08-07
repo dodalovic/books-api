590ef59610e35f2e45cf31647a97f581902d9693
Dusan <dodalovic@gmail.com>
2019-08-07 21:52:12 +0200

    No need for var

--- src/main/kotlin/rs/dodalovic/kotlin/booksapi/books/persistence.kt
+++ src/main/kotlin/rs/dodalovic/kotlin/booksapi/books/persistence.kt
@@ -6,10 +6,10 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Document("books")
-class Book(@Id var id: String? = null, val name: String, val authorId: String)
+class Book(@Id val id: String? = null, val name: String, val authorId: String)
 
 @Document("authors")
-class Author(@Id var id: String? = null, val name: String)
+class Author(@Id val id: String? = null, val name: String)
 
 @Repository
 interface BooksRepo : CrudRepository<Book, String>

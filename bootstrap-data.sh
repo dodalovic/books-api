745a78e993e6c0c8f2f1b49c982530fabb91b16a
Dusan <dodalovic@gmail.com>
2019-08-10 16:46:23 +0200

    Remove unused

--- bootstrap-data.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl -d '{"name":"John"}' -H "Content-Type: application/json" -X POST http://localhost:8080/authors
-curl -d '{"name":"Jeanny"}' -H "Content-Type: application/json" -X POST http://localhost:8080/authors
-curl -d '{"name":"Marry"}' -H "Content-Type: application/json" -X POST http://localhost:8080/authors

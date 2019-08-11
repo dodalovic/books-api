99ecbb69b3ead0d454f95164bc88fe6828d7b8e1
Dusan <dodalovic@gmail.com>
2019-08-11 20:06:10 +0200

    Fix val

--- src/main/kotlin/rs/dodalovic/kotlin/booksapi/books/persistence.kt
+++ src/main/kotlin/rs/dodalovic/kotlin/booksapi/books/persistence.kt
@@ -6,10 +6,10 @@ import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Document("books")
-class Book(@Id val id: String? = null, val name: String, val authorId: String)
+class Book(@Id var id: String? = null, val name: String, val authorId: String)
 
 @Document("authors")
-class Author(@Id val id: String? = null, val name: String)
+class Author(@Id var id: String? = null, val name: String)
 
 @Repository
 interface BooksRepo : PagingAndSortingRepository<Book, String>

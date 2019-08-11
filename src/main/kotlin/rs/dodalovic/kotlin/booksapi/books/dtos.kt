de48cb14bb3a54b3c1f5987429b0dd2e92cd12ff
Dusan <dodalovic@gmail.com>
2019-08-11 20:06:23 +0200

    Add @Size validation

--- src/main/kotlin/rs/dodalovic/kotlin/booksapi/books/dtos.kt
+++ src/main/kotlin/rs/dodalovic/kotlin/booksapi/books/dtos.kt
@@ -1,6 +1,8 @@
 package rs.dodalovic.kotlin.booksapi.books
 
-data class NewAuthor(val name: String)
+import javax.validation.constraints.Size
+
+data class NewAuthor(@field:Size(min = 3) val name: String)
 data class AuthorDTO(val id: String, val name: String)
-data class NewBook(val name: String, val authorId: String)
+data class NewBook(@field:Size(min = 3) val name: String, val authorId: String)
 data class BookDTO(val id: String, val name: String, val author: AuthorDTO)
